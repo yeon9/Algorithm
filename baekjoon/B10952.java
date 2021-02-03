2b1cf46b5ed53c3f5ec4182a1cfe381f74836374
RoYeonJu <husn0504@naver.com>
2021-02-04 00:17:26 +0900

    2021.02.04

--- baekjoon/B10952.java
+++ baekjoon/B10952.java
@@ -3,7 +3,6 @@ import java.io.*;
 public class B10952 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-     //  BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         while(true){
             String line = br.readLine();
